2833d5ae67f37bb92f7a18ff7b88501682bdcf63
Jan Werner <mail@janwerner.de>
2017-05-25 17:35:14 +0200

    changes to startup.sh / mycron

--- assets/startup.sh
+++ assets/startup.sh
@@ -1,44 +1,25 @@
 #!/bin/sh
 
-#Check if Webgrab++ config file exists in config folder. Copy to config if not existing.
+#Check if Webgrab++ config file exists in config folder. Copy to config if non-existent.
 if [ ! -f /config/WebGrab++.config.xml ]; then
   cp /webgrab/assets/WebGrab++.config.xml /config/
 fi
 
-#Check if mdb folder exists in config folder. Copy to config if not existing.
+#Check if mdb folder exists ...
 if [ ! -d /config/mdb ]; then
   cp -R /webgrab/mdb /config/ && rm -f /config/mdb/*.example.xml
 fi
 
-#Check if rex folder exists in config folder. Copy to config if not existing.
+#Check if rex folder exists ...
 if [ ! -d /config/rex ]; then
   cp -R /webgrab/rex /config/ && rm -f /config/rex/*.example.xml
 fi
 
-#Symlink WebGrab++ config
-if [ -f /config/WebGrab++.config.xml ]; then
-  ln -s /config/WebGrab++.config.xml /webgrab/WebGrab++.config.xml
+#Check if any INI file exists. Copy default (prisma.de.ini) if non-existent
+count=`ls -1 /config/*.ini 2>/dev/null | wc -l`
+if [ $count = 0 ]; then
+  cp -R /webgrab/siteini.pack/Germany/prisma.de.ini /config/ && rm -f /config/rex/*.example.xml
 fi
-
-#Symlink MDB config
-if [ -f /config/mdb.config.xml ]; then
-  rm -f /webgrab/mdb/
-  ln -s /config/mdb /webgrab/mdb
-fi
-
-#Symlink REX config
-if [ -f /config/rex.config.xml ]; then
-  rm -f /webgrab/rex/
-  ln -s /config/rex /webgrab/rex
-fi
-
-#Create log file if it doesn't exist
-if [ ! -f /data/WebGrab++.log.txt ]; then
-  touch /data/WebGrab++.log.txt && chown nobody /data/WebGrab++.log.txt
-fi
-
-#Symlink log file
-ln -sf /data/WebGrab++.log.txt /webgrab/WebGrab++.log.txt
 
 #Check if user modified cron file exists and move to wg++.
 if [ -e /config/mycron ]; then
@@ -49,7 +30,7 @@ fi
 chown -R nobody:users /config
 chown -R nobody:users /data
 
-#Start an update on container start if config file exist
+#Start an update on container start if config file exists
 if [ -f /config/WebGrab++.config.xml ]; then
-  sudo -u nobody /webgrab/run.sh
+  sudo -u nobody mono /webgrab/bin/WebGrab+Plus.exe "/config"
 fi
